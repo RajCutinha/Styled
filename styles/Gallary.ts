34fe35477d39ae51fe645885bc5c248fa75805c8
Raj Cutinha <rajath.cutinha@protonmail.com>
2023-04-02 23:51:38 +0200

    refactor: remove old project files

--- styles/Gallary.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Gallery = styled.div`
-  display: grid;
-  grid-auto-flow: row dense;
-  grid-template-rows: masonry;
-  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-  gap: 1rem;
-`;
